120ccb63b858441b0ca937beaceac6d9e93c25f5
steffakasid <github@steffen-rumpf.de>
2022-02-27 21:41:23 +0100

    feat: At Oled to print out CO2 leel

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,21 @@
 #include <Arduino.h>
+
+// WLan
+#include <ESP8266WiFi.h>
+
+// CO2 Sensor
 #include <MHZ19.h>
 #include <SoftwareSerial.h>
+
 #include <NeoPixel.h>
 
+// OLED
 #include <U8g2lib.h> // U8glib library for the OLED you download below
 #include <Wire.h>    // Set Wire library for I2C communication
 #include <SPI.h>
 
 #define CLK_PIN D1
-#define VID_PIN D1
+#define VID_PIN D2
 
 U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(/* reset=*/U8X8_PIN_NONE, CLK_PIN, VID_PIN);
 
@@ -35,62 +42,61 @@ unsigned long getDataTimer = 0;
 
 void setup()
 {
+  Serial.begin(9600);
+
+  // Disable WLan
+  WiFi.mode( WIFI_OFF );
+  WiFi.forceSleepBegin();
+
+  // Init NeoPixel
   neoPixel.begin();
   neoPixel.setBrightness(NEO_BRIGHTNESS);
 
+  // Init CO2 Sensor
   Serial.begin(BAUDRATE);
   mySerial.begin(BAUDRATE);
   myMHZ19.begin(mySerial);
   myMHZ19.autoCalibration(false);
 
-  Serial.begin(9600);
+  // Init OLED
   u8x8.begin();
   u8x8.setPowerSave(0);
   u8x8.setFont(u8x8_font_amstrad_cpc_extended_f);    
-
-  u8x8.inverse();
+  u8x8.setFlipMode(1);
 }
 
 void loop()
 {
   if (millis() - getDataTimer >= 2000)
   {              
-    int CO2 = myMHZ19.getCO2();
-    u8x8.println("CO2 (ppm) "+ CO2);
+    int co2 = myMHZ19.getCO2();
+    char buffer [50];
+    sprintf (buffer, "%d ppm", co2);
+    u8x8.draw2x2String(5, 0, "CO2");
+    u8x8.draw2x2String(0, 5, buffer);
 
     int stepsGreen = (GREEN_BOUND - LOWER_BOUND) / NEO_PIXEL_RING_SIZE;
     int stepsOrange = (ORANGE_BOUND - GREEN_BOUND) / NEO_PIXEL_RING_SIZE;
     int stepsRed = (RED_BOUND - ORANGE_BOUND) / NEO_PIXEL_RING_SIZE;
 
-    if (CO2 < LOWER_BOUND)
+    int numPixels = 0;
+    if (co2 < LOWER_BOUND)
     {
       neoPixel.theaterChase(neoPixel.Color(0, 255, 0), 10);
     }
-    else if (CO2 < GREEN_BOUND)
+    else if (co2 < GREEN_BOUND)
     {
-      int numPixels = (CO2 - LOWER_BOUND) / stepsGreen;
-      Serial.print("Number of NEO Pixels ");
-      u8x8.print("Number of NEO Pixels ");
-      Serial.println(numPixels);
-      u8x8.println(numPixels);
+      numPixels = (co2 - LOWER_BOUND) / stepsGreen;
       neoPixel.setColor(neoPixel.Color(0, 255, 0), numPixels, 50);
     }
-    else if (CO2 < ORANGE_BOUND)
+    else if (co2 < ORANGE_BOUND)
     {
-      int numPixels = (CO2 - GREEN_BOUND) / stepsOrange;
-      Serial.print("Number of NEO Pixels ");
-      u8x8.print("Number of NEO Pixels ");
-      Serial.println(numPixels);
-      u8x8.println(numPixels);
+      numPixels = (co2 - GREEN_BOUND) / stepsOrange;
       neoPixel.setColor(neoPixel.Color(255, 255, 0), numPixels, 50);
     }
-    else if (CO2 < RED_BOUND)
+    else if (co2 < RED_BOUND)
     {
-      int numPixels = (CO2 - ORANGE_BOUND) / stepsRed;
-      Serial.print("Number of NEO Pixels ");
-      u8x8.print("Number of NEO Pixels ");
-      Serial.println(numPixels);
-      u8x8.println(numPixels);
+      numPixels = (co2 - ORANGE_BOUND) / stepsRed;
       neoPixel.setColor(neoPixel.Color(255, 0, 0), numPixels, 50);
     }
     else
@@ -100,7 +106,11 @@ void loop()
 
     delay(MEASURE_DELAY);
 
-    Serial.printf("CO2 (ppm): %d\n", CO2);
+    Serial.print("Number of NEO Pixels ");
+    Serial.println(numPixels);
+
+    Serial.printf("CO2 (ppm): %d\n", co2);
+
     getDataTimer = millis();
   }
 }
